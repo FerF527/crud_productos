26508180ce4c1f11f1e17bba9b4d115203ef6a05
FerF527 <fernandofarias527@gmail.com>
2024-10-12 18:22:09 -0300

    index de productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -11,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Product::all());
     }
 
     /**
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Support\Facades\Storage;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Product extends Model
+class Product
 {
-    use HasFactory;
+    private static $disk = "local";
+    private static $file = "product.json";
+
+    public static function all()
+    {
+        if (!Storage::disk(self::$disk)->exists(self::$file)) {
+            Storage::disk(self::$disk)->put(self::$file,"[]");
+        }
+        return Storage::disk(self::$disk)->json(self::$file);
+    }
+
 }
