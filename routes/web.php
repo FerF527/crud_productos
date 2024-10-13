b8041d8330151e9e0456b24fc2e93dce0d5f087e
FerF527 <fernandofarias527@gmail.com>
2024-10-13 14:59:12 -0300

    se agrega tabla de productos

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('product');
 });
 
 Route::resource('product', ProductController::class);
